240610cb609b1f2625bf4388f21dd27e52df5418
Rickylaboa <rickylaboa@hotmail.it>
2019-05-15 10:46:21 +0200

    included pellegrini's benchmark

---
+++ benchmark/fiber.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#define USERSPACE
+
+#ifdef USERSPACE
+
+#include "src/ult.h"
+
+#define ConvertThreadToFiber() ult_convert()
+#define CreateFiber(dwStackSize, lpStartAddress, lpParameter) ult_creat(dwStackSize, lpStartAddress, lpParameter)
+#define SwitchToFiber(lpFiber) ult_switch_to(lpFiber)
+#define FlsAlloc(lpCallback) fls_alloc()
+#define FlsFree(dwFlsIndex)	fls_free(dwFlsIndex)
+#define FlsGetValue(dwFlsIndex) fls_get(dwFlsIndex)
+#define FlsSetValue(dwFlsIndex, lpFlsData) fls_set((dwFlsIndex), (long long)(lpFlsData))
+
+#else
+
+
+#include "../libs/userfibers.h"
+
+#define ConvertThreadToFiber() convert_thread_to_fiber()
+#define CreateFiber(dwStackSize, lpStartAddress, lpParameter) create_fiber(dwStackSize, (void*) lpStartAddress, (void*) lpParameter)
+#define SwitchToFiber(lpFiber) switch_to_fiber((long) lpFiber)
+#define FlsAlloc(lpCallback) fls_alloc()
+#define FlsFree(dwFlsIndex) fls_free((long) dwFlsIndex)
+#define FlsGetValue(dwFlsIndex) fls_get_value((long) dwFlsIndex)
+#define FlsSetValue(dwFlsIndex, lpFlsData) fls_set_value((long) dwFlsIndex, (void*) lpFlsData)
+
+#endif /* USERSPACE */
