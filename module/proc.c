241211eb8180af0a6215853fd66fd002330e3433
fabmx <fab92mar@gmail.com>
2019-05-30 14:32:02 +0200

    small restyle of proc.c

--- module/proc.c
+++ module/proc.c
@@ -2,7 +2,7 @@
 #include<linux/string.h>
 #include<linux/namei.h>
 #include<linux/kallsyms.h>
-#include <includes/fiber_struct.h>
+#include<includes/fiber_struct.h>
 
   /* Lookup of proc tigid/pident functions
   - proc_fill_cache: > fills (IF POSSIBLE) a directory entry, putting into /proc/[pid] one 
@@ -27,15 +27,15 @@
 static unsigned long cr0;
 static struct pid_entry* tgid_base_stuff;
 static int (*proc_link)(struct dentry *, struct path *);
-static int (*_proc_tgid_base_readdir)(struct file*,struct dir_context*);
-static int (*proc_tgid_base_readdir)(struct file*,struct dir_context*);
-static struct dentry* (*proc_tgid_base_lookup)(struct inode*,struct dentry*, unsigned int);
+static int (*_proc_tgid_base_readdir)(struct file*, struct dir_context*);
+static int (*proc_tgid_base_readdir)(struct file*, struct dir_context*);
+static struct dentry* (*proc_tgid_base_lookup)(struct inode*, struct dentry*, unsigned int);
 static int (*proc_pident_readdir)(struct file*, struct dir_context*, const struct pid_entry*, unsigned int);
 static struct dentry* (*proc_pident_lookup)(struct inode*, struct dentry*, const struct pid_entry*, unsigned int);
 //static struct dentry* (*proc_pident_instantiate)(struct inode *dir, struct dentry *dentry,struct task_struct *task, const void *ptr);
-struct inode* (*proc_pid_make_inode)(struct super_block * sb,struct task_struct *task, umode_t mode);
+struct inode* (*proc_pid_make_inode)(struct super_block * sb, struct task_struct *task, umode_t mode);
 int* (*pid_revalidate)(struct dentry *dentry, unsigned int flags);
-void* (*task_dump_owner)(struct task_struct *task, umode_t mode,kuid_t *ruid, kgid_t *rgid);
+void* (*task_dump_owner)(struct task_struct *task, umode_t mode, kuid_t *ruid, kgid_t *rgid);
 void* (*security_task_to_inode)(struct task_struct *p, struct inode *inode);
 
 
@@ -43,10 +43,7 @@ static struct file_operations* proc_tgid_base_operations;
 static struct inode_operations* proc_tgid_base_inode_operations;
 static struct inode_operations proc_pid_link_inode_operations;
 static struct dentry_operations* pid_dentry_operations;
-
 static const struct inode_operations f_proc_iops;
-
-
 
 
 static inline void protect(void)
@@ -78,12 +75,12 @@ static inline struct task_struct *get_proc_task(const struct inode *inode)
 /*  Function used as a parser to read the specific sequential
     file of a single fiber within a process.*/
 static int show_fiber_file(struct seq_file* file, void* f){
-  struct fiber_struct* fiber;
+  struct fiber_struct *fiber;
   long index;
 
   fiber = (struct fiber_struct*) file->private;
   index = fiber->index;
-  seq_printf(file, "%s: %ld\n", "Fiber",index); 
+  seq_printf(file, "%s: %ld\n", "Fiber", index); 
   return 0;
 }
 
@@ -93,8 +90,8 @@ static int show_fiber_file(struct seq_file* file, void* f){
     of the file and to allow any subsequent reading. */
 static int fibered_file_open(struct inode *inode, struct file *file){
   int res;
-  unsigned long pid,index;
-  struct fiber_struct* fiber;
+  unsigned long pid, index;
+  struct fiber_struct *fiber;
 
   res = kstrtoul(file->f_path.dentry->d_name.name, 10, &index);
   if(res != 0){
@@ -104,9 +101,9 @@ static int fibered_file_open(struct inode *inode, struct file *file){
   if(res != 0){
     return -1;
   }
-  fiber = get_fiber_pid(pid,index);
+  fiber = get_fiber_pid(pid, index);
   if(!fiber) printk("Null pointer\n");
-  res = single_open(file,show_fiber_file,fiber);
+  res = single_open(file, show_fiber_file, fiber);
   return res;
 }
 
@@ -158,11 +155,11 @@ static struct dentry *proc_pident_instantiate(struct dentry *dentry,
   allowing to show correctly all the fibers of the process, if present.*/
 static int fibers_folder_readdir(struct file *file, struct dir_context *ctx) {
   char buf[64];
-  int res, i=0;
-  int size;
-  char* pid_s; 
-  char* name;
-  struct pid_entry* entries;
+  int res, size;
+  int i = 0;
+  char *pid_s; 
+  char *name;
+  struct pid_entry *entries;
 
 
   long unsigned int pid;
@@ -174,49 +171,51 @@ static int fibers_folder_readdir(struct file *file, struct dir_context *ctx) {
     return 0;
   }
   size = number_of_fibers(pid);
-  entries = kzalloc(size*sizeof(struct pid_entry),GFP_KERNEL);
-  if(!entries) return 0;
+  entries = kzalloc(size*sizeof(struct pid_entry), GFP_KERNEL);
+  if(!entries){
+    return 0;
+  }
 
-  for(i=0; i < size; i++)
+  for(i = 0; i < size; i++)
   {  
-    res = snprintf(buf,64,"%d",i);
-    name = kmalloc(res+1,GFP_KERNEL);
-    memcpy(name,buf,res+1);
+    res = snprintf(buf, 64, "%d", i);
+    name = kmalloc(res + 1, GFP_KERNEL);
+    memcpy(name, buf, res+1);
     entries[i].name = name;
     entries[i].len = res;
     entries[i].mode = S_IFREG | S_IRUGO | S_IWUGO;
     entries[i].iop = &f_proc_iops;
     entries[i].fop = &f_proc_ops;
   }
-  res = proc_pident_readdir(file,ctx,entries,size);
-  for(i=0; i < size; i++)
+  res = proc_pident_readdir(file, ctx, entries, size);
+  for(i = 0; i < size; i++)
   {
     kfree(entries[i].name);
   }
   kfree(entries);
+
   return res;
 }
-
 
 
 /*  Function allowing the lookup into /proc/[pid]/fibers directory,
     if present. */
 static struct dentry *fibers_folder_lookup(struct inode *dir, struct dentry *dentry, unsigned int flags){    
-  struct dentry* ret;
+  struct dentry *ret;
   char buf[64];
-  char* pid_s;
+  char *pid_s;
   long unsigned int pid;
-  int res, i=0;
-  int size;
-  struct dentry* actual_d;
-  struct dentry* parent_d;
-  char* name;
-  struct pid_entry* entries;
+  int res, size;
+  int i = 0;
+  struct dentry *actual_d;
+  struct dentry *parent_d;
+  char *name;
+  struct pid_entry *entries;
 
 
   actual_d = container_of(dir->i_dentry.first, struct dentry, d_u.d_alias);
-  parent_d = actual_d -> d_parent;
-  pid_s = parent_d -> d_iname; 
+  parent_d = actual_d-> d_parent;
+  pid_s = parent_d-> d_iname; 
 
 
   res = kstrtoul(pid_s, 10, &pid);
@@ -225,26 +224,29 @@ static struct dentry *fibers_folder_lookup(struct inode *dir, struct dentry *den
   }
   
   size = number_of_fibers(pid);
-  entries = kzalloc(size*sizeof(struct pid_entry),GFP_KERNEL);
-  if(!entries) return NULL;
+  entries = kzalloc(size*sizeof(struct pid_entry), GFP_KERNEL);
+  if(!entries){
+    return NULL;
+  }
 
-  for(i=0; i < size; i++)
+  for(i = 0; i < size; i++)
   {           
-    res = snprintf(buf,64,"%d",i);
-    name = kmalloc(res+1,GFP_KERNEL);
-    memcpy(name,buf,res+1);
+    res = snprintf(buf, 64, "%d", i);
+    name = kmalloc(res + 1, GFP_KERNEL);
+    memcpy(name, buf, res+1);
     entries[i].name = name;
     entries[i].len = res;
     entries[i].mode = S_IFREG | S_IRUGO | S_IWUGO;
     entries[i].iop = &f_proc_iops;
     entries[i].fop = &f_proc_ops;
   }
-  ret = proc_pident_lookup(dir, dentry,entries,size);
-  for(i=0; i < size; i++)
+  ret = proc_pident_lookup(dir, dentry,entries, size);
+  for(i = 0; i < size; i++)
   {
     kfree(entries[i].name);
   }
   kfree(entries);
+
   return ret;
 }
 
@@ -268,10 +270,10 @@ static const struct pid_entry fiber_base_stuff[] = {
 
 /* Function allowing the reading of the /proc/[pid] directory, replacing
   proc_tgid_base_readdir, in order to show also the /fibers folder if present.*/
-static int f_proc_tgid_base_readdir(struct file* file,struct dir_context* ctx)
+static int f_proc_tgid_base_readdir(struct file *file, struct dir_context *ctx)
 {
-  struct inode* dir;
-  struct task_struct* task;
+  struct inode *dir;
+  struct task_struct *task;
   int pid, num_fibers;
   int fiber_pident;
   int actual_pident;
@@ -284,7 +286,7 @@ static int f_proc_tgid_base_readdir(struct file* file,struct dir_context* ctx)
   }else{
     fiber_pident = 0;
   }
-  actual_pident = proc_tgid_base_readdir(file,ctx);
+  actual_pident = proc_tgid_base_readdir(file, ctx);
 
   return fiber_pident && actual_pident;
 }
@@ -300,7 +302,9 @@ static struct dentry *f_proc_pident_lookup(struct inode *dir,
   int pid;
   int num_fibers;
 
-	if (!task) return res;
+	if (!task){
+     return res;
+  }
   pid = task->tgid;
   num_fibers = number_of_fibers(pid);
   if(num_fibers > 0){
@@ -316,16 +320,16 @@ static struct dentry *f_proc_pident_lookup(struct inode *dir,
   the /proc/[pid]/fibers folder. */
 static struct dentry *f_proc_lookup(struct inode *dir, struct dentry *dentry, unsigned int flags)
 {
-  struct dentry* res;
+  struct dentry *res;
   int size;
-  res = proc_tgid_base_lookup(dir,dentry,flags);
-  if(res!=ERR_PTR(-ENOENT)){
+  res = proc_tgid_base_lookup(dir, dentry, flags);
+  if(res != ERR_PTR(-ENOENT)){
     return res;
   }
-  res = f_proc_pident_lookup(dir,dentry,fiber_base_stuff);
+  res = f_proc_pident_lookup(dir, dentry, fiber_base_stuff);
+
   return res;
 }
-
 
 
 
@@ -334,21 +338,21 @@ void proc_init(){
   cr0 = read_cr0();
   _proc_tgid_base_readdir = (void *) kallsyms_lookup_name("proc_tgid_base_readdir");
   proc_tgid_base_readdir = (void *) kallsyms_lookup_name("proc_tgid_base_readdir");
-  proc_link = (void*) kallsyms_lookup_name("proc_root_link");
-  proc_tgid_base_lookup = (void*) kallsyms_lookup_name("proc_tgid_base_lookup");
-  proc_pident_readdir = (void*) kallsyms_lookup_name("proc_pident_readdir");
-  proc_pident_lookup = (void*) kallsyms_lookup_name("proc_pident_lookup");
+  proc_link = (void *) kallsyms_lookup_name("proc_root_link");
+  proc_tgid_base_lookup = (void *) kallsyms_lookup_name("proc_tgid_base_lookup");
+  proc_pident_readdir = (void *) kallsyms_lookup_name("proc_pident_readdir");
+  proc_pident_lookup = (void *) kallsyms_lookup_name("proc_pident_lookup");
   //proc_pident_instantiate = (void*) kallsyms_lookup_name("proc_pident_instantiate");
-  proc_pid_make_inode = (void*)  kallsyms_lookup_name("proc_pid_make_inode");
-  task_dump_owner = (void*) kallsyms_lookup_name("task_dump_owner");
-  security_task_to_inode = (void*) kallsyms_lookup_name("security_task_to_inode");
+  proc_pid_make_inode = (void *)  kallsyms_lookup_name("proc_pid_make_inode");
+  task_dump_owner = (void *) kallsyms_lookup_name("task_dump_owner");
+  security_task_to_inode = (void *) kallsyms_lookup_name("security_task_to_inode");
 
   /* Lookup of proc tgid operations*/
-  tgid_base_stuff = (struct pid_entry*) kallsyms_lookup_name("tgid_base_stuff");
-  pid_dentry_operations = (struct dentry_operations*) kallsyms_lookup_name("pid_dentry_operations");
-  proc_tgid_base_operations = (struct file_operations*) kallsyms_lookup_name("proc_tgid_base_operations");
-  proc_tgid_base_inode_operations = (struct inode_operations*) kallsyms_lookup_name("proc_tgid_base_inode_operations");
-  proc_pid_link_inode_operations = *(struct inode_operations*) kallsyms_lookup_name("proc_tgid_base_inode_operations");
+  tgid_base_stuff = (struct pid_entry *) kallsyms_lookup_name("tgid_base_stuff");
+  pid_dentry_operations = (struct dentry_operations *) kallsyms_lookup_name("pid_dentry_operations");
+  proc_tgid_base_operations = (struct file_operations *) kallsyms_lookup_name("proc_tgid_base_operations");
+  proc_tgid_base_inode_operations = (struct inode_operations *) kallsyms_lookup_name("proc_tgid_base_inode_operations");
+  proc_pid_link_inode_operations = *(struct inode_operations *) kallsyms_lookup_name("proc_tgid_base_inode_operations");
 
 
   unprotect();
