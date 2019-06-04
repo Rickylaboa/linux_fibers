e4db9ec5051a126d765cef1157f291fd9c3611e8
Rickylaboa <rickylaboa@hotmail.it>
2019-06-05 01:30:02 +0200

    re-put "|" in benchmark

--- benchmark/src/main.c
+++ benchmark/src/main.c
@@ -100,6 +100,7 @@ static void main_loop(void *args) {
 	// Notify other fibers that I'm done and reduce the total time
 	__sync_fetch_and_add(&exec_millis, millis);
 	__sync_fetch_and_sub(&completed_fibers, 1);
+	printf("|");
 	if(id != 1) {
 		while(true) {
 			schedule(0);
